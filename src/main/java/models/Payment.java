8e5f481cff2a559f5f1be8413ef27134efd5c9b5
Kapil Garg <gargkapil90@gmail.com>
2024-03-09 23:57:03 +0530

    updating entities

--- src/main/java/models/Payment.java
+++ src/main/java/models/Payment.java
@@ -1,4 +1,14 @@
 package models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Payment  extends BaseModel{
+    double amount;
+    String refNumber;
+    PaymentStatus paymentStatus;
+    PaymentProvider paymentProvider;
+    PaymentMode paymentMode;
 }
---
+++ src/main/java/models/PaymentMode.java
@@ -0,0 +1,9 @@
+package models;
+
+public enum PaymentMode {
+    DEBIT_CARD,
+    CREDIT_CARD,
+    UPI,
+    NET_BANKING,
+    DISCOUNT_COUPON
+}
